5d93948db3b3dd9a522ab4e76a92980c00238bd1
zeroedinat <zeroedinat@gmail.com>
2016-09-07 15:14:10 -0500

    Added comments and Removed unused code
    Added comments to each java file
    Removed unused import statements from the MediaPlayerView.fxml.

--- src/MediaPlayerMVC/MediaFile.java
+++ src/MediaPlayerMVC/MediaFile.java
@@ -3,7 +3,12 @@ package MediaPlayerMVC;
 import java.io.Serializable;
 
 /**
- * Created by Jonathan on 8/30/2016.
+ * Created by Jonathan on 8/29/2016.
+ *
+ * This class will contain the necessary information for a file being
+ * used in the MediaPlayer. It also makes use of the Comparable and Serializable
+ * interfaces so that the class can be used in Collections and be written to a
+ * binary file.
  */
 public class MediaFile implements Comparable<MediaFile>, Serializable
 {
@@ -13,6 +18,14 @@ public class MediaFile implements Comparable<MediaFile>, Serializable
 
     protected String absolutePath;
 
+    /**
+     * This constructor will take in values for the extension, name, and
+     * absolute path for a given file.
+     *
+     * @param type          The extension of the given file.
+     * @param name          The name of the given file.
+     * @param absolutePath  The absolute path of the given file.
+     */
     public MediaFile(String type, String name, String absolutePath)
     {
         this.type = type;
@@ -20,7 +33,14 @@ public class MediaFile implements Comparable<MediaFile>, Serializable
         this.absolutePath = absolutePath;
     }
 
-
+    /**
+     * This method overrides the compareTo method to ensure that any sorting done
+     * with MediaFiles is done according to the name property.
+     *
+     * @param otherFile The other MediaFile object being used for comparison.
+     *
+     * @return          The int result of the comparison.
+     */
     @Override
     public int compareTo(MediaFile otherFile)
     {
--- src/MediaPlayerMVC/MediaPlayerModel.java
+++ src/MediaPlayerMVC/MediaPlayerModel.java
@@ -7,8 +7,21 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+
+/**
+ * Created by Jonathan Oler on 8/29/2016.
+ *
+ * This class is the main starting point of the application and
+ * will just define the primary GUI stage and link an FXML and CSS file to
+ * it.
+ */
 public class MediaPlayerModel extends Application
 {
+    /**
+     * This method will define the primary GUI stage and link an FXML and CSS file to it.
+     * @param primaryStage The primary stage for the GUI
+     * @throws Exception   Any uncaught exceptions that occur during the running of the application will be thrown.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Pane pane = FXMLLoader.load(getClass().getResource("MediaPlayerView.fxml"));
@@ -23,6 +36,11 @@ public class MediaPlayerModel extends Application
         primaryStage.show();
     }
 
+    /**
+     * This method is only necessary in instances where the start method cannot be called
+     * at the start of execution.
+     * @param args Any arguments needed to process the launching of the application.
+     */
     public static void main(String[] args)
     {
         launch(args);
